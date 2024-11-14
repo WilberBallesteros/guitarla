f40649728cdc6f9474b0e593018d51c8a54b5ba3
Wilber Ballesteros <ballestas01@gmail.com>
2024-11-14 18:17:12 -0500

    se muestran las guitarras con su informacion unica

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //importar el state: con el hook useState: tambien el hook de useEffect
-import { useState, useEffect } from 'react'
+import { useState} from 'react'
 
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
@@ -11,22 +11,15 @@ function App() {
   //no se pueden tener hooks de forma condicional, en un if, pues da error, no pueden estar en un loop ni en una iteracion solo estar en la parte de arriba y ya. tampoco hooks dentro de funciones
   //el state se llama en este caso data, y la funcion q lo modifica setData
 
-  const [data, setData] = useState([]) //se inicia con arreglo vacio como para traer datos de una API
+  const [data, setData] = useState(db) //se inicia con arreglo vacio como para traer datos de una API
 
-  useEffect(() => {
-    setData(db)
-  }, [])
-
-
-  //uso de useEffect: tiene un callback dentro
+  //si es una API esta es la forma mas recomendada
+    //uso de useEffect: tiene un callback dentro
   //aca dentro si se puede colocoar un if, pero los hooks siguen arriba nunca aqui adentro
   //useEffect(() => {
-  //  console.log('componente listo o escuchando por auth')
-  //}, [auth]) //si se le pasa un arreglo vacio es q no tiene dependencias y solo se ejecuta una vez cuando el componente este slito
+  //  setData(db)
+  //}, []) //si se le pasa un arreglo vacio es q no tiene dependencias y solo se ejecuta una vez cuando el componente este slito
 
-
-
-  
   return (
     <>
 
@@ -35,16 +28,14 @@ function App() {
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
-        <div className="row mt-5">
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            
-
+        <div className="row mt-5"> {/* itera 12 veces y trae 12 guitarras */}
+          {data.map((guitar) => (  
+            <Guitar
+            key={guitar.id}
+              guitar={guitar}
+            />
+          ))} 
+          
         </div>
     </main>
 
