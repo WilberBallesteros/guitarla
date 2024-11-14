f466b8d60592eacf1db5e899a21aa2c92722ca85
Wilber Ballesteros <ballestas01@gmail.com>
2024-11-14 16:41:11 -0500

    se crea archivo db con las guitarras a modo de bd, componente header creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//importar el state: con el hook useState: tambien el hook de useEffect
+import { useState, useEffect } from 'react'
+
+import Guitar from "./components/Guitar"
+import Header from "./components/Header"
+import { db } from './data/db'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  //el hoke de state siempre va aqui
+  //no se pueden tener hooks de forma condicional, en un if, pues da error, no pueden estar en un loop ni en una iteracion solo estar en la parte de arriba y ya. tampoco hooks dentro de funciones
+  //el state se llama en este caso data, y la funcion q lo modifica setData
+
+  const [data, setData] = useState([]) //se inicia con arreglo vacio como para traer datos de una API
+
+  useEffect(() => {
+    setData(db)
+  }, [])
+
+
+  //uso de useEffect: tiene un callback dentro
+  //aca dentro si se puede colocoar un if, pero los hooks siguen arriba nunca aqui adentro
+  //useEffect(() => {
+  //  console.log('componente listo o escuchando por auth')
+  //}, [auth]) //si se le pasa un arreglo vacio es q no tiene dependencias y solo se ejecuta una vez cuando el componente este slito
+
+
+
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hola Mundo en React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <Header /> {/* renderizar el componente Header */}
+
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            
+
+        </div>
+    </main>
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
     </>
   )
 }
