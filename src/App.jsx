efe0e2f564e2bd190b8c79bce64402807222d3ac
Wilber Ballesteros <ballestas01@gmail.com>
2025-03-03 11:31:07 -0500

    incrementamos cantiad de un articulo si ya estaba agregado y mostramos las guitarras en el carrrito

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,32 @@ function App() {
     //state
     //inicio el state como un arreglo vacio
     const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
 
-    console.log(db);
+    function addToCart(item) {
+
+        //retorna la posicion del indice en el arreglo si no -1
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+        if(itemExists >= 0) { //existe en el carrito
+            const updateCart = [...cart]
+            updateCart[itemExists].quantity++ //se actualiza para q si le doy agregar guitarra agregue mas
+            setCart(updateCart)
+        } else {
+            item.quantity = 1 //CANTIDAD 1 la primera q pone alpresinar el boton agregar
+            setCart([...cart, item])
+        }
+    }
 
   return (
     <>
 
-    <Header /> {//ASI COMENTO en jsx Y DE PASO ASI LLAMO UN COMPONENTE EN OTRO
-            }
+{//ASI COMENTO en jsx Y DE PASO ASI LLAMO UN COMPONENTE EN OTRO
+}
+    <Header 
+    cart={cart}
+    /> 
+    
+    
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
@@ -24,8 +42,10 @@ function App() {
             {data.map((guitar) => ( //omitimos el return porq esta implicito, ya q siempre hay un return para mostrar html
                 <Guitar
                 //el kEY SIEMPRE SE USA CUANDO UNO ITERE UNA LISTA
-                    Key={guitar.id} //se le pasa un key con un valor q no se repita, en este caso id
+                    key={guitar.id} //se le pasa un key con un valor q no se repita, en este caso id
                     guitar={guitar}//esto es un prop, prop q se llama guitar con el nombre del objeto del map guitar
+                    setCart={setCart}
+                    addToCart={addToCart}
                     
                 />
             ))}
